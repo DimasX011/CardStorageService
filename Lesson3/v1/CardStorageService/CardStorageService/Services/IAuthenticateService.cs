669236d9fefe944f015a4e4c599887a70917cf43
Dmitry Yakovlev <dimon1998daf@mail.ru>
2022-09-25 11:28:01 +0600

    Lesson3v1

---
+++ Lesson3/v1/CardStorageService/CardStorageService/Services/IAuthenticateService.cs
@@ -0,0 +1,12 @@
+﻿using CardStorageService.Models;
+using CardStorageService.Models.Requests;
+using Newtonsoft.Json;
+
+namespace CardStorageService.Services
+{
+    public interface IAuthenticateService
+    {
+        AuthenticationResponse Login(AuthenticationRequest authenticationRequest);
+        public SessionInfo GetSessionInfo(string sessionToken);
+    }
+}
