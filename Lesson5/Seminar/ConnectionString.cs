f3ef4a14af18276cc3ad69cf0499e2ff23c132b8
Dmitry Yakovlev <dimon1998daf@mail.ru>
2022-09-29 20:50:57 +0600

    Lesson5

---
+++ Lesson5/Seminar/ConnectionString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar
+{
+    public class ConnectionString
+    {
+        public string DataSourse { get; set; }
+
+        public string InitialCatalog { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Password { get; set; }
+
+        public bool MultipleActiveResultSets { get; set; }
+
+        public string App { get; set; }
+
+        public override string ToString()
+        {
+            return $"data source={DataSourse};initial catalog={InitialCatalog};User Id={UserId};Password={Password};MultipleActiveResultSets={MultipleActiveResultSets};App={App}";
+        }
+
+    }
+}
