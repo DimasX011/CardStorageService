68f51910872b9efbe1a31eb232bd33498c4203ed
Dmitry Yakovlev <dimon1998daf@mail.ru>
2022-10-07 18:27:57 +0600

    Lesson7

---
+++ Lesson7/Seminar/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Seminar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString1 = new ConnectionString
+            {
+                DataSourse = "RYAZENKA\\SQLEXPRESS",
+                InitialCatalog = "CardStorageService",
+                UserId = "CardStorageServiceUser",
+                Password = "12345",
+                MultipleActiveResultSets = true,
+                App = "EntityFramework"
+            };
+
+            List<ConnectionString> connections = new List<ConnectionString>();
+            connections.Add(connectionString1);
+           
+
+            CacheProvider cacheProvider = new CacheProvider();
+            cacheProvider.CacheConnections(connections);
+
+            connections = cacheProvider.GetConnectionsFromCache();
+
+            foreach (var connection in connections)
+                Console.WriteLine(connection);
+
+            Console.ReadKey(true);
+
+        }
+
+    }
+}
