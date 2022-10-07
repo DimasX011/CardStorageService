62cc68ed0f7de225c387242c36617214e2b0552b
Dmitry Yakovlev <dimon1998daf@mail.ru>
2022-10-07 12:21:09 +0600

    Lesson6v2

---
+++ Lesson6/v2/CardStorageService/Models/Validators/CreateClientRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using CardStorageService.Models.Requests;
+using FluentValidation;
+
+namespace CardStorageService.Models.Validators
+{
+    public class CreateClientRequestValidator : AbstractValidator<CreateClientRequest>
+    {
+        public CreateClientRequestValidator()
+        {
+            RuleFor(x => x.FirstName).NotNull().Length(3, 50).NotEqual("string");
+
+            RuleFor(x => x.Patronymic).NotNull().Length(3, 75).NotEqual("string");
+            RuleFor(x => x.Surname).NotNull().Length(3, 50).NotEqual("string");
+        }
+    }
+}
---
+++ Lesson6/v2/CardStorageService/Services/Impl/ClientService.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using CardStorageService.Data;
+using CardStorageServiceProtos;
+using Grpc.Core;
+using static CardStorageServiceProtos.ClientService;
+
+namespace CardStorageService.Services.Impl
+{
+    public class ClientService : ClientServiceBase
+    {
+        #region Services
+
+        private readonly IClientRepositoryService _clientRepositoryService;
+
+        #endregion
+
+
+        public ClientService(IClientRepositoryService clientRepositoryService, IMapper mapper)
+        {
+            _clientRepositoryService = clientRepositoryService;
+           
+        }
+
+        public override Task<CreateClientResponse> Create(CreateClientRequest request, ServerCallContext context)
+        {
+            
+            var clientId = _clientRepositoryService.Create(new Data.Client
+            {
+                FirstName = request.FirstName,
+                Surname = request.SurName,
+                Patronymic = request.Patronymic
+            });
+         
+            var response = new CreateClientResponse
+            {
+                ClientId = clientId
+            };
+
+            return Task.FromResult(response);
+        }
+
+    }
+}
